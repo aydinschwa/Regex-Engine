30f370c5d7de5a2524a7f6d2e01d7251fb4e6952
Aydin <aydinschwa@gmail.com>
2022-04-28 14:55:32 -0700

    finish draft nfa generation

--- re_to_nfa.py
+++ re_to_nfa.py
@@ -7,19 +7,19 @@ def get_formatting_states(regex):
     invisible_transitions = []
 
     for i, unit in enumerate(regex):
-        states_list.append((str(i), unit))
+        states_list.append((i, unit))
         if i > 0:
-            invisible_transitions.append((str(i - 1), str(i)))
+            invisible_transitions.append((i - 1, i))
 
     return states_list, invisible_transitions
 
 
 # build match transition digraph
 def get_match_transitions(regex):
-    match_transitions = {}
+    match_transitions = []
     for i, unit in enumerate(regex):
         if i > 0 and regex[i - 1].isalpha():
-            match_transitions[str(i - 1)] = str(i)
+            match_transitions.append((i - 1, i))
         else:
             pass
     return match_transitions
@@ -27,45 +27,55 @@ def get_match_transitions(regex):
 
 # build epsilon transition digraph
 def get_epsilon_transitions(regex):
-    epsilon_transitions = {}
+    epsilon_transitions = []
+    operator_idx_stack = []
     for i, unit in enumerate(regex):
-        if i > 0 and unit in "()":
-            epsilon_transitions[str(i - 1)] = str(i)
-        # TODO: implement single-character and expression closures
-        elif unit == "*":
-            epsilon_transitions[str(i - 1)] = str(i)
-            epsilon_transitions[str(i)] = str(i - 1)
+        if unit == "(" or unit == "|":
+            operator_idx_stack.append(i)
+        elif unit == ")":
+            op_idx = operator_idx_stack.pop(-1)
+            if regex[op_idx] == "|":
+                left_paren_idx = operator_idx_stack.pop(-1)
+                epsilon_transitions.append((left_paren_idx, op_idx + 1))
+                epsilon_transitions.append((op_idx, i))
+            else:
+                left_paren_idx = op_idx
+        if (i < (len(regex) - 1)) and regex[i + 1] == "*":
+            epsilon_transitions.append((i, i + 1))
+            epsilon_transitions.append((i + 1, i))
 
+        if unit in "(*)" and i < len(regex) - 1:
+            epsilon_transitions.append((i, i + 1))
+
+    print(epsilon_transitions)
     return epsilon_transitions
 
 
 def draw_nfa(gv_states, gv_edges, match_transitions, epsilon_transitions):
     graph = gv.Digraph(comment="NFA")
-    graph.attr(rankdir="LR")
+    graph.attr(rankdir="LR", ordering="in")
 
     # add states
-    [graph.node(tup[0], tup[1]) for tup in gv_states]
+    [graph.node(str(tup[0]), str(tup[1]), rank="same") for tup in gv_states]
 
     # add invisible edges for proper node ordering
-    [graph.edge(tup[0], tup[1], style="invis") for tup in gv_edges]
+    [graph.edge(str(tup[0]), str(tup[1]), style="invis") for tup in gv_edges]
 
     # add match transition edges
-    edges = [(key, value) for key, value in match_transitions.items()]
-    graph.edges(edges)
+    [graph.edge(str(tup[0]), str(tup[1]), color="black", constraint="false", rank="max") for tup in match_transitions]
 
     # add epsilon transition edges
-    edges = [(key, value) for key, value in epsilon_transitions.items()]
-    [graph.edge(tup[0], tup[1], color="red") for tup in edges]
+    [graph.edge(str(tup[0]), str(tup[1]), color="red", constraint="false") for tup in epsilon_transitions]
 
     # add invisible edges for proper node ordering
-    [graph.edge(tup[0], tup[1], style="invis") for tup in gv_edges]
+    [graph.edge(str(tup[0]), str(tup[1]), style="invis") for tup in gv_edges]
 
     graph.render('output/nfa').replace('\\', '/')
 
 
 alphabet = "A B C D".split()
 metacharacters = "( ) . * |".split()
-test_re = "((A*B|AC)D)"
+test_re = "((A*B|AC(AD)D)"
 
 gv_states, gv_edges = get_formatting_states(test_re)
 match_edge_dict = get_match_transitions(test_re)
