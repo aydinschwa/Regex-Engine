dcaf49951013a071f43bfcfa3732061ed1a618fb
Aydin <aydinschwa@gmail.com>
2022-05-04 10:47:46 -0700

    improve graphviz formatting

--- re_to_nfa.py
+++ re_to_nfa.py
@@ -17,7 +17,7 @@ def get_formatting_states(regex):
 
 # build match transition digraph
 def get_match_transitions(regex):
-    # create dictionary the can support keys with multiple edges
+    # create dictionary that can support keys with multiple edges
     match_transitions = defaultdict(list)
     for i, unit in enumerate(regex):
         if i > 0 and regex[i - 1].isalpha():
@@ -27,9 +27,15 @@ def get_match_transitions(regex):
 
 
 # build epsilon transition digraph
+# need to create mini dictionaries of specific edges for graphviz formatting
 def get_epsilon_transitions(regex):
     # create dictionary the can support keys with multiple edges
     epsilon_transitions = defaultdict(list)
+
+    # make a bunch of smaller edge dictionaries for graphviz formatting
+    star_dict = {"N": [], "S": []}
+    closure_dict = {"(": [], "|": []}
+
     operator_idx_stack = []
     for i, unit in enumerate(regex):
         left_paren_idx = i
@@ -43,30 +49,35 @@ def get_epsilon_transitions(regex):
                     or_idx_list.append(op_idx)
                 elif regex[op_idx] == "(":
                     left_paren_idx = op_idx
-                    # epsilon_transitions[left_paren_idx].append(left_paren_idx + 1)
-                    [epsilon_transitions[left_paren_idx].append(or_idx + 1) for or_idx in or_idx_list]
-                    [epsilon_transitions[or_idx].append(i) for or_idx in or_idx_list]
+                    # left parenthesis edges
+                    # [epsilon_transitions[left_paren_idx].append(or_idx + 1) for or_idx in or_idx_list]
+                    [closure_dict["("].append((left_paren_idx, or_idx + 1)) for or_idx in or_idx_list]
+
+                    # or edges
+                    # [epsilon_transitions[or_idx].append(i) for or_idx in or_idx_list]
+                    [closure_dict["|"].append((or_idx, i)) for or_idx in or_idx_list]
+
                     break
 
         if (i < (len(regex) - 1)) and regex[i + 1] == "*":
-            epsilon_transitions[left_paren_idx].append(i + 1)
-            epsilon_transitions[i + 1].append(left_paren_idx)
+            star_dict["N"].append((left_paren_idx, i + 1))
+            star_dict["S"].append((i + 1, left_paren_idx))
 
         if unit in "(*)" and i < len(regex) - 1:
             epsilon_transitions[i].append(i + 1)
 
-    return epsilon_transitions
+    return epsilon_transitions, star_dict, closure_dict
 
 
-def draw_nfa(gv_states, gv_edges, match_transitions, epsilon_transitions):
+def draw_nfa(gv_states, gv_edges, match_transitions, epsilon_transitions, star_dict, closure_dict):
     graph = gv.Digraph(comment="NFA")
     graph.attr(rankdir="LR", ranksep=".25")
 
     # add states
-    [graph.node(str(tup[0]), str(tup[1]), rank="same") for tup in gv_states]
+    [graph.node(str(tup[0]), str(tup[1])) for tup in gv_states]
 
     # add invisible edges for proper node ordering
-    [graph.edge(str(tup[0]), str(tup[1]), style="invis") for tup in gv_edges]
+    [graph.edge(str(tup[0]), str(tup[1]), style="invis", weight="10") for tup in gv_edges]
 
     # add match transition edges
     # convert from dict to list of tuples
@@ -75,7 +86,7 @@ def draw_nfa(gv_states, gv_edges, match_transitions, epsilon_transitions):
         for val in match_transitions[key]:
             match_trans_list.append((key, val))
 
-    [graph.edge(str(tup[0]), str(tup[1]), color="black", constraint="false", rank="max") for tup in match_trans_list]
+    [graph.edge(str(tup[0]) + ":e", str(tup[1]) + ":w", color="black", weight="10") for tup in match_trans_list]
 
     # add epsilon transition edges
     # convert from dict to list of tuples
@@ -83,10 +94,15 @@ def draw_nfa(gv_states, gv_edges, match_transitions, epsilon_transitions):
     for key in epsilon_transitions.keys():
         for val in epsilon_transitions[key]:
             epsilon_trans_list.append((key, val))
-    [graph.edge(str(tup[0]), str(tup[1]), color="red", constraint="false") for tup in epsilon_trans_list]
+    [graph.edge(str(tup[0]) + ":e", str(tup[1]) + ":w", color="red", weight="10") for tup in epsilon_trans_list]
 
-    # add invisible edges for proper node ordering
-    [graph.edge(str(tup[0]), str(tup[1]), style="invis") for tup in gv_edges]
+    # add * edges
+    [graph.edge(str(tup[0]) + ":ne", str(tup[1]) + ":nw", color="red") for tup in star_dict["N"]]
+    [graph.edge(str(tup[0]) + ":sw", str(tup[1]) + ":se", color="red") for tup in star_dict["S"]]
+
+    # add | closure edges
+    [graph.edge(str(tup[0]), str(tup[1]), color="red") for tup in closure_dict["("]]
+    [graph.edge(str(tup[0]), str(tup[1]), color="red") for tup in closure_dict["|"]]
 
     graph.render("output/nfa", format="png").replace("\\", "/")
 
@@ -94,12 +110,12 @@ def draw_nfa(gv_states, gv_edges, match_transitions, epsilon_transitions):
 alphabet = "A B C D".split()
 metacharacters = "( ) . * |".split()
 test_re = "((A*B|AC)D)"
-# test_re = "(A|B|C)"
+test_re = "(.*AB((C|D|E)F)*G)"
 
 gv_states, gv_edges = get_formatting_states(test_re)
 match_edge_dict = get_match_transitions(test_re)
-epsilon_edge_dict = get_epsilon_transitions(test_re)
+epsilon_edge_dict, star_dict, closure_dict = get_epsilon_transitions(test_re)
 
 print(f"match transitions: {match_edge_dict}")
 print(f"epsilon transitions: {epsilon_edge_dict}")
-draw_nfa(gv_states, gv_edges, match_edge_dict, epsilon_edge_dict)
+draw_nfa(gv_states, gv_edges, match_edge_dict, epsilon_edge_dict, star_dict, closure_dict)
